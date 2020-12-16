0b8d68426a31e3b65d86c6f52d85c6dee4f50bbf
Mubassir-Ali <mubassir11@gmail.com>
2020-12-16 15:50:36 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Cards from './Cards';
+import Sdata from './Sdata';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <React.Fragment>
+    <h1>List of Top 5 Netflix Original Series</h1>
+    <Cards
+      imgsrc={Sdata[0].imgsrc}
+      title={Sdata[0].title}
+      sname={Sdata[0].sname}
+      link={Sdata[0].link}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    />
+    <Cards
+      imgsrc={Sdata[1].imgsrc}
+      title={Sdata[1].title}
+      sname={Sdata[1].sname}
+      link={Sdata[1].link}
+    />
+
+<Cards
+      imgsrc={Sdata[2].imgsrc}
+      title={Sdata[2].title}
+      sname={Sdata[2].sname}
+      link={Sdata[2].link}
+    />
+
+<Cards
+      imgsrc={Sdata[3].imgsrc}
+      title={Sdata[3].title}
+      sname={Sdata[3].sname}
+      link={Sdata[3].link}
+    />
+
+<Cards
+      imgsrc={Sdata[4].imgsrc}
+      title={Sdata[4].title}
+      sname={Sdata[4].sname}
+      link={Sdata[4].link}
+    />
+    
+
+
+
+  </React.Fragment>,
+document.getElementById('root'));
