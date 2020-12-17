44cd743638fcbd188ebce3dbdc41d5851776aaa1
Mubassir-Ali <mubassir11@gmail.com>
2020-12-17 12:03:12 +0500

    add compomets in App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,28 @@
+import React from 'react';
+import Cards from './Cards';
+import Sdata from './Sdata';
 
+const App=()=>(
+    
+    <>
+        <h1>List of Top 5 Netflix Original Series</h1>
+    
+        {Sdata.map((val, index)=>{
+          console.log(index);
+          
+            return(
+            <Cards
+                key={val.id}
+                imgsrc={val.imgsrc}
+                title={val.title}
+                sname={val.sname}
+                link={val.link}
+            />
+            );
+        
+        })}
+    </> 
+    
+)
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Cards from './Cards';
-import Sdata from './Sdata';
+
 import './index.css';
+import App from './App'
 
-ReactDOM.render(
-  <React.Fragment>
-    <h1>List of Top 5 Netflix Original Series</h1>
-    
-    {Sdata.map(function(val){
-      return(<Cards
-        key={val.id}
-        imgsrc={val.imgsrc}
-        title={val.title}
-        sname={val.sname}
-        link={val.link}
-    
-      />
-    );
-    })}
-
-
-
-  </React.Fragment>,
-document.getElementById('root'));
+ReactDOM.render(<App/>,document.getElementById('root'));
