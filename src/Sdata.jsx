89b51f1bd7f1a7762b06f4f5defabf8807bee160
Mubassir-Ali <mubassir11@gmail.com>
2020-12-17 11:26:54 +0500

    fid error in add unique key in cards and add id in sdata

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -2,42 +2,42 @@ import React from 'react';
 
 const Sdata =[
     {
-        id=1,
+        id:1,
         imgsrc: "https://occ-0-3736-58.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABeu5Oz4CGncoVEvCDhLelkyQc7H0KsaLyauqCprkOTTePn46kiFYRe1AUg5UkCp9ka7RTgyc8mKrSy7w9VFeQDokfmzy.jpg?r=077",
         title:"A Netflix Original Series",
         sname:"Dark",
         link:"https://www.netflix.com/pk/title/80100172",
     },
     {
-        id=2,
+        id:2,
         imgsrc:"https://occ-0-3736-58.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABTXK5Gs51kpod43dfThIA0RLPIj7TPH77eZEUHWnbuH6evnIzxkxxuFd4TI1VN5rpFRyByzbH4Hq9efC4qfWevlDPvaG.jpg?r=236",
         title:"A Netflix Original Series",
         sname:"The Crown",
         link:"https://www.netflix.com/pk/title/80025678",
     },
     {
-        id=3,
+        id:3,
         imgsrc:"https://occ-0-3736-58.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABbIg52b6HD3pf9F9xO4KgT7A8rAcoqdGcQYHWv1Zs7o6L4jrpgvAdnyxyhe4AMREZQbU1M50byJjpPH5Z36mNDb-vSB2.jpg?r=220",
         title:"A Netflix Original Series",
         sname:"Lost in Space",
         link:"https://www.netflix.com/pk/title/80104198",
     },
     {
-        id=4,
+        id:4,
         imgsrc:"https://occ-0-3736-58.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABd4eoielzKfx0Vrxi0Y9bCiW5dYIbZBF3Y-Txt1NrdjGZ3L_sva5z-FyZxFvUx4mncUHx4L7n2oxg3qNnD91NhdoK6QQ.jpg?r=775",
         title:"A Netflix Original Series",
         sname:"Extraction",
         link:"https://www.netflix.com/pk/title/80230399",
     },
     {
-        id=5,
+        id:5,
         imgsrc:"https://occ-0-3736-58.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABR39sNDbLxBs8lnR5VpEZv7pIubUzM6hsHEEg9lsGdApa26_ZDOOfcwlql5iDAbKyNIPAsFAbCNhXXkx3eg2yC8VxhAM.jpg?r=d4f",
         title:"A Netflix Original Series",
         sname:"MINDHUNTER",
         link:"https://www.netflix.com/pk/title/80114855",
     },
     {
-        id=6,
+        id:6,
         imgsrc:"https://occ-0-3736-58.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABTXK5Gs51kpod43dfThIA0RLPIj7TPH77eZEUHWnbuH6evnIzxkxxuFd4TI1VN5rpFRyByzbH4Hq9efC4qfWevlDPvaG.jpg?r=236",
         title:"A Netflix Original Series",
         sname:"The Crown",
