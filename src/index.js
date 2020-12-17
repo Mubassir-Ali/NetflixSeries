2126e14944c7f9606ad4a5462fb5a3cf9bf993b5
Mubassir-Ali <mubassir11@gmail.com>
2020-12-17 11:22:40 +0500

    add unique key in cards and add id in sdata

--- src/index.js
+++ src/index.js
@@ -4,63 +4,23 @@ import Cards from './Cards';
 import Sdata from './Sdata';
 import './index.css';
 
-function ncard(val){
-  console.log(val);
-  return (<Cards
-    imgsrc={val.imgsrc}
-    title={val.title}
-    sname={val.sname}
-    link={val.link}
-    
-    />);
-};
-
-
-
-
 ReactDOM.render(
   <React.Fragment>
     <h1>List of Top 5 Netflix Original Series</h1>
     
-    {Sdata.map(ncard)}
+    {Sdata.map(function(val){
+      return(<Cards
+        key={val.id}
+        imgsrc={val.imgsrc}
+        title={val.title}
+        sname={val.sname}
+        link={val.link}
+    
+      />
+    );
+    })}
 
 
 
   </React.Fragment>,
 document.getElementById('root'));
-
-/*<Cards
-imgsrc={Sdata[0].imgsrc}
-title={Sdata[0].title}
-sname={Sdata[0].sname}
-link={Sdata[0].link}
-
-/>
-<Cards
-imgsrc={Sdata[1].imgsrc}
-title={Sdata[1].title}
-sname={Sdata[1].sname}
-link={Sdata[1].link}
-/>
-
-<Cards
-imgsrc={Sdata[2].imgsrc}
-title={Sdata[2].title}
-sname={Sdata[2].sname}
-link={Sdata[2].link}
-/>
-
-<Cards
-imgsrc={Sdata[3].imgsrc}
-title={Sdata[3].title}
-sname={Sdata[3].sname}
-link={Sdata[3].link}
-/>
-
-<Cards
-imgsrc={Sdata[4].imgsrc}
-title={Sdata[4].title}
-sname={Sdata[4].sname}
-link={Sdata[4].link}
-/>
-*/
